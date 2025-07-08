af196662bf2b6735333be8aaa55d527ac173dce2
Caty2k <kittyrobocaty123@gmail.com>
2025-07-08 18:14:16 +0000

    intro to flask

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template,request
 app=Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET','POST'])
+def init():
+    if request.method == 'GET':
+        return render_template("Home.html")
+    elif request.method == 'POST':
+        username=request.form['username']
+        return render_template("display.html", name=username)
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug = True)
